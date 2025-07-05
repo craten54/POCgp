9b85bac9ed9e9925b15c6d58c02f8338fe204989
theophilus0054 <theosamuel2005@gmail.com>
2025-07-05 15:55:27 +0700

    New : Add BattleScene + HealthBar (With Scripts)

---
+++ game vtuber/Assets/code/PlayerScript.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+    public int maxHealth = 100; // Nilai maksimum health
+    private int currentHealth; // Nilai health saat ini
+    
+
+    
+}
